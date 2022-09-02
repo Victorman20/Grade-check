4d3222a303ba9d12ffdad2f61d9c238c099e03a9
Adewole Adekunle Mercy <adewoleadekunlemercy@gmail.com>
2022-09-02 16:20:02 +0100

    first level score

---
+++ grade.js
@@ -0,0 +1,8 @@
+var maxscore = 100
+function checkscore(){
+    var myscore = inputbox.value
+    if (myscore >= 45 && myscore <=50 ){
+        alert ("Omo mi");
+        doc.innerHTML = "<h1>Omomi to sure</h1>"
+    }
+}
