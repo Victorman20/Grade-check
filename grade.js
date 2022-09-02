ac9a0cb0caad61fc6a8c487ddf9e0ac095768dd4
Adewole Adekunle Mercy <adewoleadekunlemercy@gmail.com>
2022-09-02 16:58:00 +0100

    I do from 50 to 59 grade

--- grade.js
+++ grade.js
@@ -1,8 +1,14 @@
 var maxscore = 100
 function checkscore(){
     var myscore = inputbox.value
-    if (myscore >= 45 && myscore <=50 ){
+    if (myscore >= 45 && myscore <=49 ){
         alert ("Omo mi");
-        doc.innerHTML = "<h1>Omomi to sure</h1>"
+        doc.innerHTML = "<h1>PASS</h1>"
+    };
+    
+    var myscore = inputbox.value
+    if (myscore >= 50 && myscore <=59 ){
+        alert ("Omo mi");
+        doc.innerHTML = "<h1>CREDIT</h1>";
     }
 }
